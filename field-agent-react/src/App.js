6f0fc7031ad6305d8f55af3ccba56d333c667074
Jonathon <evansj@lakeland.edu>
2022-05-16 08:30:54 -0500

    I think hopeful finish

---
+++ field-agent-react/src/App.js
@@ -0,0 +1,15 @@
+import FieldAgents from "./components/FieldAgents";
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar navbar-dark bg-danger">
+        <span className="navbar-brand" >Field Agent</span>
+      </nav>
+      <div className="container">
+        <FieldAgents />
+      </div>
+    </div>
+  );
+}
+
+export default App;
